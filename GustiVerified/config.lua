24870045a4b6903c13c286f7c786f2f06aa0af03
Gustiagung19 <Dimaasagung190399@gmail.com>
2021-04-19 01:02:11 +0800

    first commit

---
+++ GustiVerified/config.lua
@@ -0,0 +1,14 @@
+config = {}
+
+-- Name of predefined table
+config.tableName = 'GustiVerified'
+
+
+
+-- Lang (if you want to use special char like ' you have to escape them, by putting backslash \ before char)
+config.checkingInfo = 'Memeriksa Steam ID Anda, Harap Tunggu.'
+config.noEntry = 'Saya Tidak Bisa Temukan Pengenal Steam Anda, Jadi Anda\'Kembali Tidak Di Izinkan Untuk Bergabung.'
+config.tooManyWrongs = 'Anda Terlalu Sering Memasukkan Kode Salah!'
+
+-- How many times user is allowed to enter wrong code. After specified number he'll be kicked.
+config.attempts = 5
